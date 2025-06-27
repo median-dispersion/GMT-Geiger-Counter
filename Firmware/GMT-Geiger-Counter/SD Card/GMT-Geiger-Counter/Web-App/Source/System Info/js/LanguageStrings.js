b042fb0fcdf9e70ca5809e898fca065be18d8c44
median-dispersion <mediandispersion@gmail.com>
2025-06-27 10:29:12 +0200

    Added German translation to system info web app

--- Firmware/GMT-Geiger-Counter/SD Card/GMT-Geiger-Counter/Web-App/Source/System Info/js/LanguageStrings.js
+++ Firmware/GMT-Geiger-Counter/SD Card/GMT-Geiger-Counter/Web-App/Source/System Info/js/LanguageStrings.js
@@ -27,6 +27,34 @@ const language = {
         go_back:                     "Go back",
         refresh:                     "Refresh",
 
+    },
+
+    // German
+    de: {
+
+        document_title:              "Systeminformationen | GMT Geiger Counter",
+        system_info:                 "Systeminformationen",
+        system_uptime:               "Systemlaufzeit",
+        day_abbreviation:            "t",
+        since_power_on:              "seit dem Einschalten",
+        memory_usage:                "Speichernutzung",
+        bytes_of_ram_free:           "bytes Arbeitsspeicher ungenutzt",
+        system_components:           "Systemkomponenten",
+        sd_card_mounted:             "SD-Karte eingehängt",
+        geiger_counter_enabled:      "Geigerzähler aktiviert",
+        cosmic_ray_detector_enabled: "Kosmischer Strahlungsdetektor aktiviert",
+        buzzer_active:               "Lautsprecher aktiv",
+        touchscreen_active:          "Touchscreen aktiv",
+        rgb_led_active:              "RGB LED aktiv",
+        hotspot_enabled:             "Hotspot aktiviert",
+        wifi_enabled:                "WLAN aktiviert",
+        http_server_running:         "HTTP-Server aktiv",
+        firmware:                    "Firmware",
+        data_request_failed:         "Datenübertragung fehlgeschlagen!",
+        data_request_failed_message: "Die Anfrage der benötigten Daten ist fehlgeschlagen! Bitte überprüfen Sie die WLAN-Verbindung.",
+        go_back:                     "Zurück",
+        refresh:                     "Neuladen"
+
     }
 
 };
